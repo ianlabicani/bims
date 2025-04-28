b4081aaf30ca85c280e61b392d59cd56ad300a23
ianlabicani <labicaniian@gmail.com>
2025-04-28 16:14:20 +0800

    Enhance dashboard functionality: retrieve buildings based on user campus and integrate Leaflet map with satellite layer support.

--- app/Http/Controllers/Campus/DashboardController.php
+++ app/Http/Controllers/Campus/DashboardController.php
@@ -7,8 +7,20 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('campus.dashboard.index');
+        // Step 1: Check if the user has a campus
+        if (!$request->user()->campus) {
+            return redirect()->route('campus.dashboard')->with('error', 'You do not have a campus assigned.');
+        }
+
+
+        // Step 2: Get the buildings associated with the user's campus
+        $buildings = $request->user()->campus->buildings;
+
+        return view('campus.dashboard.index', [
+            'buildings' => $buildings,
+        ]);
+
     }
 }
--- resources/views/campus/dashboard/index.blade.php
+++ resources/views/campus/dashboard/index.blade.php
@@ -6,10 +6,9 @@
         <div id="map" style="height: 500px; width: 100%;"></div>
     </div>
 
-
     @push('scripts')
         <script>
-            var map = L.map('map').setView([17.6364, 121.6783], 13); // Set default view
+            var map = L.map('map').setView([17.6364, 121.6783], 13); // Default center
 
             // Initialize the streets tile layer
             var streetsLayer = L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=bZpmItn2cuWjeIdpgbH5', {
@@ -19,22 +18,25 @@
                 minZoom: 1
             }).addTo(map);
 
-            // Example: Add a marker
-            var marker = L.marker([17.6364, 121.6783]).addTo(map)
-                .bindPopup('This is Aparri Campus')
-                .openPopup();
-
-            // REMOVE the streetsLayer before adding the satellite layer
+            // Add satellite layer
             map.removeLayer(streetsLayer);
-
-            // Add the satellite layer
             const satelliteLayer = L.maptiler.maptilerLayer({
                 apiKey: 'bZpmItn2cuWjeIdpgbH5',
                 style: 'satellite',
             }).addTo(map);
+
+            // Buildings data from PHP
+            var buildings = @json($buildings);
+
+            buildings.forEach(function (building) {
+                if (building.latitude && building.longitude) {
+                    var marker = L.marker([building.latitude, building.longitude]).addTo(map)
+                        .bindPopup(
+                            '<b>' + building.name + '</b><br>' +
+                            building.address
+                        );
+                }
+            });
         </script>
-
-
     @endpush
-
 @endsection
